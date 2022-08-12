884c27fa5e07ac8ddfc2f183f9f113a1f273ead8
Amanda Sakuma, Danny Lucas <lannyducas@gmail.com>
2022-08-12 14:18:19 -0400

    commit tree f65d36f31d606031bc820b2065ce31ffe829dec7

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default function EyesOnMe() {
+  function handleFocus(){
+    console.log("Good!");
+  }
+
+  function handleBlur(){
+    console.log("Hey! Eyes on me!");
+  }
+    return (
+    <div>
+        <button onFocus={handleFocus} onBlur={handleBlur}>
+            EyesOnMe
+        </button>
+        </div>
+  )
+}
